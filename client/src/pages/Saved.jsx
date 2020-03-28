8f49ff4af6f9a2c0d4e77337bd5aa649503906d4
Joe Shields <shields.john.joseph@gmail.com>
2020-03-28 13:24:08 -0500

    created saved and search components

---
+++ client/src/pages/Saved.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Saved = () => {
+    return (
+        <div className="text-white">SAVED PAGE</div>
+    )
+}
+
+export default Saved;
---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Search = () => {
+    return (
+        <div className="container mb-5 mt-5">
+        <h1 className="text-white">SEARCH PAGE</h1>
+        </div>
+    )
+}
+
+export default Search;
