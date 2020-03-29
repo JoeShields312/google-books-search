6ee1dce1e5c6f34c8b0c6b5f54399e00801324d1
Joe Shields <shields.john.joseph@gmail.com>
2020-03-28 23:53:58 -0500

    created saved page

--- client/src/pages/Saved.jsx
+++ client/src/pages/Saved.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import Container from "../components/Container/Container.jsx";
+import React, { useState, useEffect } from "react";
+// import axios from "axios";
+import "../App.css";
+const key = "AIzaSyCJsckSxIQ8Ry42DxSvgFEeNp26mXtVDw8";
 
 
 const Saved = () => {
     return (
-        <div className="text-white">SAVED PAGE</div>
-    )
-}
+        <Container fluid>
+        <div className="searchbox text-center px-auto mx-auto">
+          <h1 className="text-white text-center">Saved PAGE</h1>
+                  <React.Fragment>
+                      <ul className="list-group px-auto mx-auto">
+                        <li className="list-group-item my-2 mx-5 px-5">
+                         
+                        </li>
+                        <li className="list-group-item my-2 mx-5 px-5">
+                          
+                        </li>
+                        <li className="list-group-item my-2 mx-5 px-5">
+                          
+                        </li>
+                        <li className="list-group-item my-2 mx-5 px-5">
+                        
+                        </li>
+                     
+                      </ul>
+                      <br />
+                      <br />
+                  </React.Fragment>
+                );
+              })
+            : ""}
+        </div>
+      </Container>
+    );
+  };
+    
+
 
 export default Saved;
