22df5134cd84b4fd77ed28d722ac360ef0233d7c
Joe Shields <shields.john.joseph@gmail.com>
2020-04-03 20:22:01 -0500

    heroku fix

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // connect to MongoDB
-const MONGODB_URI = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ds235197.mlab.com:35197/heroku_3ggmfr0c`
+// const MONGODB_URI = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ds235197.mlab.com:35197/heroku_3ggmfr0c`
 console.log(MONGODB_URI)
-mongoose.connect(MONGODB_URI || "mongodb://localhost/google-books-search", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-books-search", {
   useNewUrlParser: true, 
   useFindAndModify: false,
   useCreateIndex: true,
