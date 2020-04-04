f7f4a5a69b779e8b3b042903021f929d97040ae0
Joe Shields <shields.john.joseph@gmail.com>
2020-04-03 20:27:22 -0500

    new build

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,5 @@
 import Container from "../components/Container/Container.jsx";
 import React, { useState, useEffect } from "react";
-// import axios from "axios";
 import Api from "../utils/Api.js";
 import "../App.css";
 
