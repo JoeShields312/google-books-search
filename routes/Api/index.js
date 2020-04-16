e10def00d887d1e344b79f5347d71e17ed5a38b2
Joe Shields <shields.john.joseph@gmail.com>
2020-04-16 00:24:30 -0500

    refactored routes

--- routes/Api/index.js
+++ routes/Api/index.js
@@ -6,10 +6,4 @@ const bookRoutes = require("./books");
 // Book routes
 router.use("/books", bookRoutes);
 
-// If no API routes are hit, send the React app
-router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
-  });
-  
-
 module.exports = router;
